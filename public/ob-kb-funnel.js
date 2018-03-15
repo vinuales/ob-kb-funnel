a1a6082952e6c4d272d6ccbeb574709bdf8fed90
vinuales <vinuales@media-saturn.com>
2018-03-15 18:56:04 +0100

    Adatpation to kibana version 5.6.3

--- public/ob-kb-funnel.js
+++ public/ob-kb-funnel.js
@@ -1,21 +1,29 @@
 // Include the angular controller
-import TemplateVisTypeTemplateVisTypeProvider from 'ui/template_vis_type/template_vis_type';
-import VisSchemasProvider from 'ui/vis/schemas';
+import 'plugins/ob-kb-funnel/ob-kb-funnel.css';
+import 'plugins/ob-kb-funnel/funnelController';
+import { TemplateVisTypeProvider } from 'ui/template_vis_type/template_vis_type';
+import { VisSchemasProvider } from 'ui/vis/schemas';
+import { VisTypesRegistryProvider } from 'ui/registry/vis_types';
+import funnelVisTemplate from 'plugins/ob-kb-funnel/ob-kb-funnel.html';
+import funnelEditorVisParamsTemplate from 'plugins/ob-kb-funnel/funnelEditor.html';
 
-require('plugins/ob-kb-funnel/funnelController');
-require('plugins/ob-kb-funnel/ob-kb-funnel.css');
+VisTypesRegistryProvider.register(FunnelProvider);
 
 // The provider function, which must return our new visualization type
 function FunnelProvider(Private) {
-	const TemplateVisType = Private(TemplateVisTypeTemplateVisTypeProvider);
-    const Schemas = Private(VisSchemasProvider);
+	console.log("Dentro de FunnelProvider.");
+	console.log("TemplateVisType...");
+	const TemplateVisType = Private(TemplateVisTypeProvider);
+	console.log("Schemas...");
+	const Schemas = Private(VisSchemasProvider);
+	console.log("return TVT...");
 	// Describe our visualization
 	return new TemplateVisType({
 		name: 'obFunnel', // The internal id of the visualization (must be unique)
 		title: 'Funnel View', // The title of the visualization, shown to the user
 		description: 'Funnel visualization', // The description of this vis
 		icon: 'fa-toggle-down', // The font awesome icon of this visualization
-		template: require('plugins/ob-kb-funnel/ob-kb-funnel.html'), // The template, that will be rendered for this visualization
+		template: funnelVisTemplate,  // The template, that will be rendered for this visualization
 		// Define the aggregation your visualization accepts
 		schemas: new Schemas([
 				{
@@ -23,25 +31,25 @@ function FunnelProvider(Private) {
 					name: 'tagsize',
 					title: 'Value',
 					min: 1,
+					max: 1,
 					aggFilter: ['count', 'avg', 'sum', 'min', 'max', 'cardinality', 'std_dev']
 				},
 				{
 					group: 'buckets',
 					name: 'tags',
 					title: 'Aggregation',
-					min: 0,
+					min: 1,
 					max: 1,
 					aggFilter: '!geohash_grid'
 				}
 			]),
 		params: {
-			editor: require('plugins/ob-kb-funnel/funnelEditor.html'),
+			editor: funnelEditorVisParamsTemplate,
 			defaults: {
 				absolute: true,
       			percent: false,
       			percentFromTop: false,
       			percentFromAbove: false,
-      			sumOption: 'byBuckets',
       			funnelOptions : "\
 {\n\
   \"block\": { \n\
@@ -60,4 +68,4 @@ function FunnelProvider(Private) {
 	});
 }
 
-require('ui/registry/vis_types').register(FunnelProvider);
+export default FunnelProvider;
